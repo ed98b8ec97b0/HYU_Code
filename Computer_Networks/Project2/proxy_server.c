71db47e4346e7fbf80467cd36db7283ef2bf67d7
장호연 <hoyeon@MacBook.local>
2017-11-28 16:39:32 +0900

    first proxy_server

--- Computer_Networks/Project2/proxy_server.c
+++ Computer_Networks/Project2/proxy_server.c
@@ -6,10 +6,12 @@
 #include <strings.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <netdb.h> 
 
 #define BACKLOG 5
 #define BUFF_SIZE 5120
 #define CASH_SIZE 10
+#define NOT_FOUND "HTTP/1.1 404 NOT Found\r\n\r\n"
 
 void error(char *msg) {
     perror(msg);
@@ -17,11 +19,13 @@ void error(char *msg) {
 }
 
 int main(int argc, char* argv[]) {
-    int proxy_sock, serv_sock, cli_sock;
+    int proxy_sock, cli_sock;
     int port_no;
     int n;
-    char buffer[BUFF_SIZE];
+    char buffer[BUFF_SIZE], tmp[BUFF_SIZE];
+    char *token = NULL, *url = NULL;
     struct sockaddr_in proxy_addr, serv_addr, cli_addr;
+    struct hostent *server;
     socklen_t clilen;
 
     if (argc < 2) {
@@ -55,7 +59,66 @@ int main(int argc, char* argv[]) {
     }
 
     while(1) {
-        clilen = sizeof(client_address);
+        clilen = sizeof(cli_addr);
+
+        // accepting client socket
+        cli_sock = accept(proxy_sock, (struct sockaddr *) &proxy_addr, &clilen);
+        if (cli_sock < 0) {
+            error("ERROR accept client socket");
+        }
+
+        // reset buffer
+        memset(buffer, 0, BUFF_SIZE);
+
+        // read client socket
+        n = read(cli_sock, buffer, BUFF_SIZE-1);
+        if (n < 0) {
+            error("ERROR read client socket");
+        }
+
+        // get url from http request
+        memcpy(tmp, buffer, BUFF_SIZE);
+        token = strtok(tmp, " ");
+        url = strtok(NULL, " ");
+
         
+        // connet to server
+        server = gethostbyname(url);
+        if (server == NULL) {
+            n = write(cli_sock, NOT_FOUND, 27);
+            if (n < 0) {
+                error("ERROR write client socket");
+            }
+        }
+        memset((char *) &serv_addr, 0, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        memcpy((char *) &serv_addr.sin_addr.s_addr, (char *) server->h_addr, server->h_length);
+        serv_addr.sin_port = 80;
+
+        n = connect(proxy_sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+        if (n < 0) {
+            error("ERROR connect server");
+        }
+        
+        n = write(proxy_sock, buffer, BUFF_SIZE);
+        if (n < 0) {
+            error("ERROR write proxy");
+        }
+
+        n = read(proxy_sock, buffer, BUFF_SIZE);
+        if (n < 0) {
+            error("ERROR read proxy");
+        }
+
+        n = write(cli_sock, buffer, BUFF_SIZE);
+        if (n < 0) {
+            error("ERROR wrtie client socket");
+        }
+
+        memset(buffer, 0, BUFF_SIZE);
+        close(cli_sock);
+        close(proxy_sock);
     }
+
+    return 0;
 }
