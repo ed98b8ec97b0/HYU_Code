e5e01e2236bf01b5979c10f53e7d45aa68cd2613
장호연 <hoyeon@MacBook.local>
2017-11-29 17:34:13 +0900

    none while betweeen read and write

--- Computer_Networks/Project2/proxy_server.c
+++ Computer_Networks/Project2/proxy_server.c
@@ -12,30 +12,36 @@
 #define BUFF_SIZE 5120
 #define CASH_SIZE 10
 #define NOT_FOUND "HTTP/1.1 404 NOT Found\r\n\r\n"
-void error(char *msg) {
+
+void error(char *msg)
+{
     perror(msg);
     exit(1);
 }
 
-int main(int argc, char* argv[]) {
-    int proxy_sock, cli_sock;
+int main(int argc, char* argv[])
+{
+    int proxy_sock, cli_sock, serv_sock;
     int port_no;
-    int n;
+    int n, m;
     char buffer[BUFF_SIZE], tmp[BUFF_SIZE];
-    char* token = NULL;
+    char* token1 = NULL;
+    char* token2 = NULL;
     char* url;
     struct sockaddr_in proxy_addr, serv_addr, cli_addr;
     struct hostent *server;
     socklen_t clilen;
 
-    if (argc < 2) {
+    if (argc < 2)
+    {
         fprintf(stderr, "ERROR argument");
         exit(1);
     }
 
     // proxy server socket open
     proxy_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (proxy_sock < 0) {
+    if (proxy_sock < 0)
+    {
         error("ERROR proxy socket");
     }
     
@@ -49,22 +55,26 @@ int main(int argc, char* argv[]) {
 
     // binding proxy socket
     n = bind(proxy_sock, (struct sockaddr *) & proxy_addr, sizeof(proxy_addr));
-    if (n < 0) {
+    if (n < 0)
+    {
         error("ERROR bind proxy socket");
     }
 
     // proxy socket into listening mode 
     n = listen(proxy_sock, BACKLOG);
-    if (n < 0) {
+    if (n < 0)
+    {
         error("ERROR listen proxy socket");
     }
 
-    while(1) {
+    while(1)
+    {
         clilen = sizeof(cli_addr);
 
         // accepting client socket
         cli_sock = accept(proxy_sock, (struct sockaddr *) &proxy_addr, &clilen);
-        if (cli_sock < 0) {
+        if (cli_sock < 0)
+        {
             error("ERROR accept client socket");
         }
 
@@ -73,21 +83,25 @@ int main(int argc, char* argv[]) {
 
         // read client socket
         n = read(cli_sock, buffer, BUFF_SIZE-1);
-        if (n < 0) {
+        if (n < 0)
+        {
             error("ERROR read client socket");
         }
 
         // get url from http request
         memcpy(tmp, buffer, BUFF_SIZE);
-        token = strtok(tmp, " ");
-        token = strtok(NULL, " ");
-        n = strlen(token);
+        token1 = strtok(tmp, " ");
+        token1 = strtok(NULL, " ");
+        token2 = strtok(token1, "/");
+        token2 = strtok(NULL, "/");
+        n = strlen(token2);
         url = (char *) malloc(sizeof(char) * n);
-        memmove(url, token, n-1);
+        memcpy(url, token2, n);
 
         // connet to server
-        server = gethostbyname(url+7);
-        if (server == NULL) {
+        server = gethostbyname(url);
+        if (server == NULL)
+        {
             n = write(cli_sock, NOT_FOUND, 27);
             if (n < 0) {
                 error("ERROR write client socket");
@@ -99,29 +113,41 @@ int main(int argc, char* argv[]) {
         memcpy((char *) &serv_addr.sin_addr.s_addr, (char *) server->h_addr, server->h_length);
         serv_addr.sin_port = htons(80);
 
-        n = connect(proxy_sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-        if (n < 0) {
+        // proxy server socket open
+        serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (serv_sock < 0)
+        {
+            error("ERROR proxy socket");
+        }
+
+        n = connect(serv_sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+        if (n < 0)
+        {
             error("ERROR connect server");
         }
 
-        n = write(proxy_sock, buffer, BUFF_SIZE);
-        if (n < 0) {
+        n = write(serv_sock, buffer, BUFF_SIZE);
+        if (n < 0)
+        {
             error("ERROR write proxy");
         }
-
-        n = read(proxy_sock, buffer, BUFF_SIZE);
-        if (n < 0) {
+        
+        n = read(serv_sock, buffer, BUFF_SIZE);
+        if (n < 0)
+        {
             error("ERROR read proxy");
         }
 
-        n = write(cli_sock, buffer, BUFF_SIZE);
-        if (n < 0) {
+        m = write(cli_sock, buffer, n);
+        if (m < 0)
+        {
             error("ERROR wrtie client socket");
         }
 
         memset(buffer, 0, BUFF_SIZE);
         close(cli_sock);
         close(proxy_sock);
+        close(serv_sock);
     }
 
     return 0;
