4f2360a6ad060784fb92bca261550407adbb2bc4
Hoyeon <ed98b8ec97b0@gmail.com>
2017-11-29 22:56:47 +0900

    Revert "del free, close out of while"
    This reverts commit 9ad59ae891e8584ae3a6b33f661e121008f158aa.

--- Computer_Networks/Project2/proxy_server.c
+++ Computer_Networks/Project2/proxy_server.c
@@ -170,10 +170,10 @@ int main(int argc, char* argv[])
         }
         
         memset(buffer, 0, BUFF_SIZE);
-        // free(url);
+        free(url);
+        close(serv_sock);
+        close(cli_sock);
     }
-    close(serv_sock);
-    close(cli_sock);
     close(proxy_sock);
 
     return 0;
