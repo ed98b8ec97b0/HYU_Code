1cf90b0e1183f2385c9d854b1cee35a55ed21fe9
Hoyeon <ed98b8ec97b0@gmail.com>
2017-11-30 16:40:25 +0900

    LRU header

---
+++ Computer_Networks/Project2/lru.h
@@ -0,0 +1,25 @@
+#include <string.h>
+
+#define CASH_SIZE 6164480
+
+typedef struct object
+{
+    // object
+    char* url;
+    char* path;
+    int position;
+    char* buffer;
+
+    // queue
+    object* next;
+} object
+
+typedef struct queue
+{
+    int size;
+    object* front;
+    object* back;
+    object* header;
+} queue
+
+queue* cache = (queue*)malloc(sizeof(queue));
