9070c107feccadff320fd48e55484b20f436ddf5
Hoyeon <ed98b8ec97b0@gmail.com>
2017-12-01 02:43:52 +0900

    del func without hit, check, miss & add print_cache

--- Computer_Networks/Project2/lru.c
+++ Computer_Networks/Project2/lru.c
@@ -1,22 +1,56 @@
 #include "lru.h"
 
-object* hit(char* url, char* path)
+object* hit(queue* cache, char* url, char* path)
 {
     object* iter = cache->front;
-    do
+    object *first, *temp;
+    int length;
+
+    // when cache->front is hitted
+    if ((strcmp(iter->url, url) == 0) && (strcmp(iter->path, path) == 0))
     {
-        if ((strcmp(iter->url, url) == 0) && (strcmp(iter->path, path) == 0))
+        first = iter;
+        length = first->full_length;
+        do
         {
-            break;
+            cache->front = iter->next;
+            iter->next = NULL;
+            cache->back->next = iter;
+            cache->back = iter;
+            iter = cache->front;
+            length -= iter->length;
+        } while (length > 0);
+    }
+    // when object in midlle of cache is hitted (* also cache->back)
+    else
+    {
+        while(iter->next != NULL)
+        {
+            if ((strcmp(iter->next->url, url) == 0) && (strcmp(iter->next->path, path) == 0))
+            {
+                break;
+            }
+            iter = iter->next;
         }
 
-        iter = iter->next;
-    } while (iter->next != NULL);  
+        first = iter->next;
+        length = first->full_length;
+        temp = first;
+        do
+        {
+            iter->next = temp->next;
+            temp->next = NULL;
+            cache->back->next = temp;
+            cache->back = temp;
+            temp = iter->next;
+            length -= iter->length;
+        } while (length > 0);
+    }
 
-    return iter;
+    return first;
 }
 
-int check(char* url, char* path)
+int check(queue *cache, char *url, char *path)
 {
     int flag = 0; // 1 = hit, 0 = miss.
 
@@ -30,7 +64,7 @@ int check(char* url, char* path)
     object* iter = cache->front;
     do
     {
-        if ((strcmp(iter->url, url) == 0) && (strcmp(iter->path, path) == 0))
+        if (!(strcmp(iter->url, url)) && !(strcmp(iter->path, path)))
         {
             flag = 1;
             break;
@@ -42,49 +76,80 @@ int check(char* url, char* path)
     return flag;
 }
 
-void header_move(int n)
+void miss(queue *cache, object *first)
 {
-    for (int i = 0; i < n; i++)
+    object *iter, *temp;
+
+    // first input at cache.
+    if (cache->size == 0)
     {
-        if (cache->header->next != NULL)
+        cache->size += first->full_length;
+        cache->front = first;
+
+        iter = first;
+        while(iter->next != NULL)
         {
-            cache->header = cache->header->next;
+            iter = iter->next;
         }
-        else
+        
+        cache->back = iter;
+
+        return;
+    }
+    
+    if (((CASH_SIZE - cache->size) - first->full_length) >= 0)
+    {
+        cache->size += first->full_length;
+        cache->back->next = first;
+
+        iter = first;
+        while (iter->next != NULL)
         {
-            cache->header = cache->front;
+            iter = iter->next;
+        }
+
+        cache->back = iter;
+    }
+    else
+    {
+        while (((CASH_SIZE - cache->size) - first->full_length) < 0)
+        {
+            cache->size -= cache->front->full_length;
+            
+            iter = cache->front;
+            while (iter->position != 0)
+            {
+                temp = iter;
+                iter = iter->next;
+                free(temp);
+            }
+            cache->front = iter;
         }
     }
 }
 
-void miss(object* obje)
+void print_cache(queue *cache)
 {
-    object* prev = cache->header;
-    object* temp1 = prev->next;
-    object* temp2 = temp1->next;
+    object *iter;
 
-    for(int i = 0; i < obje->count; i++)
+    printf("\n[");
+
+    if (cache->size == 0)
     {
-        free(temp1);
-        temp1 = temp2;
-        temp2 = temp1->next;
+        printf("(null)");
     }
-
-    prev->next = obje;
-    header_move(n);
-}
-
-void c_free(object* first)
-{
-    object* temp1 = first;
-    object* temp2 = first->next;
-    for (int i = 0; i < first->count; i++)
+    else
     {
-        free(temp1);
-        temp1 = temp2;
-        if (temp1->next != NULL)
+        iter = cache->front;
+        while(iter->next != NULL)
         {
-            temp2 = temp1->next;
+            if (iter->position == 0)
+            {
+                printf("-(%s/%s)-", iter->url, iter->path);
+            }   
+            iter = iter->next;
         }
     }
+
+    printf("]\n");
 }
