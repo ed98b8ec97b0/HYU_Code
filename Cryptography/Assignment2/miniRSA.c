fbbf5205ac6e1f1413d2fb5cc9aa54a34f691f2f
Hoyeon <ed98b8ec97b0@gmail.com>
2017-12-02 11:53:49 +0900

    Cyptography Assignment #2

---
+++ Cryptography/Assignment2/miniRSA.c
@@ -0,0 +1,147 @@
+/*
+ * @file    rsa.c
+ * @author  작성자 이름 / 학번
+ * @date    작성 일자
+ * @brief   mini RSA implementation code
+ * @details 세부 설명
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "miniRSA.h"
+
+uint p, q, e, d, n;
+
+/*
+ * @brief     모듈러 덧셈 연산을 하는 함수.
+ * @param     uint a     : 피연산자1.
+ * @param     uint b     : 피연산자2.
+ * @param     byte op    : +, - 연산자.
+ * @param     uint n      : 모듈러 값.
+ * @return    uint result : 피연산자의 덧셈에 대한 모듈러 연산 값. (a op b) mod n
+ * @todo      모듈러 값과 오버플로우 상황을 고려하여 작성한다.
+ */
+uint ModAdd(uint a, uint b, byte op, uint n) {
+    return result
+}
+
+/*
+ * @brief      모듈러 곱셈 연산을 하는 함수.
+ * @param      uint x       : 피연산자1.
+ * @param      uint y       : 피연산자2.
+ * @param      uint n       : 모듈러 값.
+ * @return     uint result  : 피연산자의 곱셈에 대한 모듈러 연산 값. (a x b) mod n
+ * @todo       모듈러 값과 오버플로우 상황을 고려하여 작성한다.
+ */
+uint ModMul(uint x, uint y, uint n) {
+    return result
+}
+
+/*
+ * @brief      모듈러 거듭제곱 연산을 하는 함수.
+ * @param      uint base   : 피연산자1.
+ * @param      uint exp    : 피연산자2.
+ * @param      uint n      : 모듈러 값.
+ * @return     uint result : 피연산자의 연산에 대한 모듈러 연산 값. (base ^ exp) mod n
+ * @todo       모듈러 값과 오버플로우 상황을 고려하여 작성한다.
+               'square and multiply' 알고리즘을 사용하여 작성한다.
+ */
+uint ModPow(uint base, uint exp, uint n) {
+    return result
+}
+
+/*
+ * @brief      입력된 수가 소수인지 입력된 횟수만큼 반복하여 검증하는 함수.
+ * @param      uint testNum   : 임의 생성된 홀수.
+ * @param      uint repeat    : 판단함수의 반복횟수.
+ * @return     uint result    : 판단 결과에 따른 TRUE, FALSE 값.
+ * @todo       Miller-Rabin 소수 판별법과 같은 확률적인 방법을 사용하여,
+               이론적으로 4N(99.99%) 이상 되는 값을 선택하도록 한다. 
+ */
+bool IsPrime(uint testNum, uint repeat) {
+    return result
+}
+
+/*
+ * @brief       모듈러 역 값을 계산하는 함수.
+ * @param       uint a      : 피연산자1.
+ * @param       uint m      : 모듈러 값.
+ * @return      uint result : 피연산자의 모듈러 역수 값.
+ * @todo        확장 유클리드 알고리즘을 사용하여 작성하도록 한다.
+ */
+uint ModInv(uint a, uint m) {
+    return result
+}
+
+/*
+ * @brief     RSA 키를 생성하는 함수.
+ * @param     uint *p   : 소수 p.
+ * @param     uint *q   : 소수 q.
+ * @param     uint *e   : 공개키 값.
+ * @param     uint *d   : 개인키 값.
+ * @param     uint *n   : 모듈러 n 값.
+ * @return    void
+ * @todo      과제 안내 문서의 제한사항을 참고하여 작성한다.
+ */
+void miniRSAKeygen(uint *p, uint *q, uint *e, uint *d, uint *n) {
+}
+
+/*
+ * @brief     RSA 암복호화를 진행하는 함수.
+ * @param     uint data   : 키 값.
+ * @param     uint key    : 키 값.
+ * @param     uint n      : 모듈러 n 값.
+ * @return    uint result : 암복호화에 결과값
+ * @todo      과제 안내 문서의 제한사항을 참고하여 작성한다.
+ */
+uint miniRSA(uint data, uint key, uint n) {
+    return result;
+}
+
+uint GCD(uint a, uint b) {
+    uint prev_a;
+
+    while(b != 0) {
+        printf("GCD(%u, %u)\n", a, b);
+        prev_a = a;
+        a = b;
+        while(prev_a >= b) prev_a -= b;
+        b = prev_a;
+    }
+    printf("GCD(%u, %u)\n\n", a, b);
+    return a;
+}
+
+int main(int argc, char* argv[]) {
+    byte plain_text[4] = {0x12, 0x34, 0x56, 0x78};
+    uint plain_data, encrpyted_data, decrpyted_data;
+    uint seed = time(NULL);
+
+    memcpy(&plain_data, plain_text, 4);
+
+    // 난수 생성기 시드값 설정
+    seed = time(NULL);
+    InitWELLRNG512a(&seed);
+
+    // RSA 키 생성
+    miniRSAKeygen(&p, &q, &e, &d, &n);
+    printf("0. Key generation is Success!\n ");
+    printf("p : %u\n q : %u\n e : %u\n d : %u\n N : %u\n\n", p, q, e, d, n);
+
+    // RSA 암호화 테스트
+    encrpyted_data = miniRSA(plain_data, e, n);
+    printf("1. plain text : %u\n", plain_data);    
+    printf("2. encrypted plain text : %u\n\n", encrpyted_data);
+
+    // RSA 복호화 테스트
+    decrpyted_data = miniRSA(encrpyted_data, d, n);
+    printf("3. cipher text : %u\n", encrpyted_data);
+    printf("4. Decrypted plain text : %u\n\n", decrpyted_data);
+
+    // 결과 출력
+    printf("RSA Decryption: %s\n", (decrpyted_data == plain_data) ? "SUCCESS!" : "FAILURE!");
+
+    return 0;
+}
