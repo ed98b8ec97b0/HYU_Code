1c65b81d4de3e875e673fe13a09d577053451063
Hoyeon <ed98b8ec97b0@gmail.com>
2017-12-18 21:47:38 +0900

    Last commit smsh

--- System_Programming/homework4/README.md
+++ System_Programming/homework4/README.md
@@ -4,13 +4,7 @@
 ## 개요
 smsh 파일을 실행하면 다음 명령어들을 사용할 수 있다.
 - foreground and background execution (&)
-- multiple commands separated by semicolons (;)
 - history command (history)
-- shell redirection(>, >>, >! <)
-- shell pipe (Command A | Command B)
-- multiple pipe (Command A | Command B | Command C ...)
 - change directory command (cd ..)
 
-위에 없는 명령어는 사용할 수 없다.
-
-## 함수 설명
+위 항목에 없는 명령어는 모두 /bin/(명령어)에 있는 것을 사용한다.
--- System_Programming/homework4/parse_cmd.c
+++ System_Programming/homework4/parse_cmd.c
@@ -10,11 +10,14 @@ command* parse_cmd(char *cmd) {
     output->argc = 0;
     output->bg_flag = 0;
 
+    //맨 마지막에 bg가 있는지 확인.
     if (cmd[strlen(cmd) - 2] == '&') {
         output->bg_flag = 1;
+        // 마지막에 &가 있다면 bg_flag를 1로 바꾸고 &을 null로 바꿔 일반적으로 사용할 수 있게 만든다.
         cmd[strlen(cmd) - 2] = '\0';
     }
 
+    //tokenizing
     tmp = strtok(cmd, " \n");
     while (tmp != NULL) {
         output->argv[output->argc] = tmp;
@@ -22,6 +25,7 @@ command* parse_cmd(char *cmd) {
         tmp = strtok(NULL, " \n");
     }
 
+    // argument가 한개일 경우 마지막이 \n이 아니라 null이 들어가도록 하는것.
     if (output->argc == 1) {
         tmp = strtok(output->argv[0], "\0");
         output->argv[0] = tmp;
