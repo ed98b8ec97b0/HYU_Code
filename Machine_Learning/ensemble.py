446fdb1518b9bb8970578d0a1e2f6bf23ce541c0
Hoyeon <ed98b8ec97b0@gmail.com>
2017-12-22 05:50:48 +0900

    No ensemble

--- Machine_Learning/ensemble.py
+++ Machine_Learning/ensemble.py
@@ -14,6 +14,7 @@ from sklearn.decomposition import PCA
 from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import VotingClassifier
 
 
 
@@ -34,10 +35,10 @@ def load_mnist(path, kind):
     return images, labels
 
 # Load Data
-# print('=== Loading MNIST Data ===')
+print('=== Loading MNIST Data ===')
 X_train, y_train = load_mnist('mnist/', 'train')
 X_test, y_test = load_mnist('mnist/', 'test')
-# print('Compelte!')
+print('Compelte!')
 
 # Neural Network
 nn = MLPClassifier(hidden_layer_sizes=(800,),
@@ -49,40 +50,36 @@ nn = MLPClassifier(hidden_layer_sizes=(800,),
                     max_iter=100,
                     shuffle=True,
                     random_state=1,
-                    verbose=False)
+                    verbose=True)
 
 # Pipeline
-# print('=== Fitting Classifier ===')
-pipe_nn = Pipeline([('scl', StandardScaler()),
+print('=== Fitting Classifier ===')
+clf = Pipeline([('scl', StandardScaler()),
                     ('pca', PCA(n_components=250)),
                     ('clf', nn)])
 
-pipe_nn.fit(X_train, y_train)
-# print('Training Compeleted!')
+eclf = VotingClassifier(estimators=[('mlp1', clf), ('mlp2', clf), ('mlp3', clf), ('mlp4', clf), ('mlp5', clf), ('mlp6', clf), ('mlp7', clf), ('mlp8', clf), ('mlp9', clf), ('mlp10', clf), ('mlp11', clf)], voting='hard')
+eclf.fit(X_train, y_train)
+print('Training Compeleted!')
 
+# Testing
+print('=== Testing Classifier ===')
+y_train_pred = eclf.predict(X_train)
 
+if sys.version_info < (3, 0):
+    acc = ((np.sum(y_train == y_train_pred, axis=0)).astype('float') /
+           X_train.shape[0])
+else:
+    acc = np.sum(y_train == y_train_pred, axis=0) / X_train.shape[0]
 
-# Acccuracy
-# print('=== Testing Classifier ===')
-print('Training Accuracy: %.2f%%' % (pipe_nn.score(X_train, y_train) * 100))
-print('Test Accuracy:     %.2f%%' % (pipe_nn.score(X_test, y_test) * 100))
+print('Training Accuracy: %.2f%%' % (acc * 100))
 
-# y_train_pred = pipe_nn.predict(X_train)
+y_test_pred = eclf.predict(X_test)
 
-# if sys.version_info < (3, 0):
-#     acc = ((np.sum(y_train == y_train_pred, axis=0)).astype('float') /
-#            X_train.shape[0])
-# else:
-#     acc = np.sum(y_train == y_train_pred, axis=0) / X_train.shape[0]
+if sys.version_info < (3, 0):
+    acc = ((np.sum(y_test == y_test_pred, axis=0)).astype('float') /
+           X_test.shape[0])
+else:
+    acc = np.sum(y_test == y_test_pred, axis=0) / X_test.shape[0]
 
-# print('Training Accuracy: %.2f%%' % (acc * 100))
-
-# y_test_pred = pipe_nn.predict(X_test)
-
-# if sys.version_info < (3, 0):
-#     acc = ((np.sum(y_test == y_test_pred, axis=0)).astype('float') /
-#            X_test.shape[0])
-# else:
-#     acc = np.sum(y_test == y_test_pred, axis=0) / X_test.shape[0]
-
-# print('Test Accuracy: %.2f%%' % (acc * 100))
+print('Test Accuracy: %.2f%%' % (acc * 100))
