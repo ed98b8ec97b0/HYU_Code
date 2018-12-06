5d059d5dc86a902f579a3b622ef7b6ef7a795746
ed98b8ec97b0 <ed98b8ec97b0@gmail.com>
2018-12-07 03:50:15 +0900

    ITE4067 PathFinder

--- ITE4067/test.py
+++ ITE4067/test.py
@@ -5,53 +5,95 @@ from ev3dev2.motor import LargeMotor, MediumMotor
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.sound import Sound
+import threading, time
 
-SENSOR_SPEED = 5
+SENSOR_SPEED = 5.3
 SENSOR_TIME = 1
-PAPER_SPEED = 5
-PAPER_TIME = 1.5
+PAPER_SPEED = 6
+PAPER_TIME = 1
 MAP = ['무', '검', '파', '초', '노', '빨', '흰', '갈']
+status = False
+line = 0
 
 pm = LargeMotor(OUTPUT_A)
 cp = LargeMotor(OUTPUT_B)
 cs = ColorSensor(INPUT_1)
-cs.mode = 'COL-COLOR'
-map = []
+cs.mode = 'RGB-RAW'
+all_map = []
+in_map = []
 
-def move_left(n):
+def move_left(n=1):
     cp.on_for_seconds(-SENSOR_SPEED, SENSOR_TIME * n)
 
-def move_right(n):
+def move_right(n=1):
     cp.on_for_seconds(SENSOR_SPEED, SENSOR_TIME * n)
 
-def inroll(n):
+def inroll(n=1):
     pm.on_for_seconds(PAPER_SPEED, PAPER_TIME * n)
 
-def outroll(n):
+def outroll(n=1):
     pm.on_for_seconds(-PAPER_SPEED, PAPER_TIME * n)
 
-def map_print(map):
-    for i in range(len(map)):
-        print(MAP[map[i]])
+def map_print(target=all_map):
+    for i in range(len(target)):
+        print(target[i])
 
-def scan_map(map):
-    for i in range(0, 9):
-        map.append([])
-        for _ in range(0, 9):
-            map[i].append(cs.value())
-            if (i % 2 == 0):
-                move_right(1)
-            else:
-                move_left(1)
-            map_print(map)
+def scan_color(status):
+    while(status):
+        all_map[line].append(MAP[cs.value()])
+        time.sleep(1)
+
+def color_picker(rgb):
+    if (rgb <= 207 and rgb >= 201):
+        return "W"
+    elif (rgb <= 21 and rgb >= 15):
+        return "B"
+    elif (rgb <= 58 and rgb >= 52):
+        return "S"
+    elif (rgb <= 179 and rgb >= 171):
+        return "E"
+
+def start():
+    for _ in range(5):
+        for _ in range(9):
+            in_map.append(cs.value())
+            move_left()
+
+        in_map.append(cs.value())
+        print(in_map)
+        in_map.clear()
         inroll(1)
 
-# initial
-# move_left(9)
+        for _ in range(9):
+            in_map.append(cs.value())
+            move_right()
 
-# scan
-# scan_map(map)
+        in_map.append(cs.value())
+        print(in_map)
+        in_map.clear()
+        inroll(1)
+    outroll(10)
 
-# check
-move_left(9)
-# move_right(9)
+
+# start()
+inroll(10)
+
+# Red
+# MAX: 179
+# MIN: 171
+# Mean: 174.06
+
+# Green
+# Max: 58
+# Min: 52
+# Mean: 54.84
+
+# Black
+# Max: 21
+# Min: 15
+# Mean: 18.2
+
+# White
+# Max: 207
+# Min: 201
+# Mean: 203.06
