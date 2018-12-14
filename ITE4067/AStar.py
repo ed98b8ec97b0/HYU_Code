9d606569e8f1dd5015e9debb56cc4680aa871212
ed98b8ec97b0 <ed98b8ec97b0@gmail.com>
2018-12-14 18:08:51 +0900

    MazeRunner

---
+++ ITE4067/AStar.py
@@ -0,0 +1,81 @@
+import itertools, operator, copy
+
+class AStar:
+    def __init__(self, init, target, graph):
+        self.graph = graph
+        self.init = init
+        self.target = target
+        self.result = []
+
+    def h(self, head):
+        return abs(head[0] - self.target[0]) + abs(head[1] - self.target[1])        
+
+    def f(self, head, history):
+        return len(history) + self.h(head)
+
+    def close(self, x, y):
+        wall = {}
+        for i, j in itertools.product(range(x), range(y)):
+            if self.graph[i][j]==1:
+                wall[(i, j)] = 1
+            else:
+                wall[(i, j)] = 0
+        return wall
+    
+    def find_direction(self, head, wall):
+        x, y = head
+        direction = {}
+        if (x != 0):
+            direction[(-1, 0)] = wall[(x-1, y)]
+        if (x != len(self.graph)):
+            direction[(1, 0)] = wall[(x+1, y)]
+        if (y != 0):
+            direction[(0, -1)] = wall[(x, y-1)]
+        if (y != len(self.graph[0])):
+            direction[(0, 1)] = wall[(x, y+1)]
+        return direction
+
+    def search(self, head, wall, history):
+        if head == self.target:
+            print("{}[{}]: Complete!\n{}\n".format(head, len(history), history))
+            self.result.append((len(history), history))
+            return
+        if head == self.init:
+            print("{}[{}]: Start!\n{}\n".format(head, self.f(head, history), history))
+        direction = self.find_direction(head, wall)
+        move_cost = {}
+        for d in direction:
+            if direction[d] == 0:
+                future = tuple(map(operator.add, head, d))
+                move_cost[d] = self.f(head, history)
+        if bool(move_cost):
+            sorted_cost = sorted(move_cost.items(), key=lambda kv: kv[1])
+            for s in dict(sorted_cost):
+                future = tuple(map(operator.add, head, s))
+                new_wall = copy.deepcopy(wall)
+                new_wall[head] = 1
+                new_history = copy.deepcopy(history)
+                new_history.append(head)
+                print("{}[{}] -> {}[{}]:\n{}\n".format(head, self.f(head, history), future, self.f(future, new_history), new_history))
+                self.search(future, new_wall, new_history)
+        else:
+            print("{}[{}]: No Way!\n{}\n".format(head, self.f(head, history), history))
+            
+
+
+if __name__ == "__main__":
+    graph = [[1, 3, 1, 1, 1, 1, 1, 1, 1, 1],
+             [1, 6, 6, 1, 6, 6, 6, 1, 6, 1],
+             [1, 6, 1, 6, 6, 1, 6, 6, 6, 1],
+             [1, 6, 1, 1, 6, 6, 6, 1, 1, 1],
+             [1, 6, 6, 6, 6, 1, 6, 6, 6, 1],
+             [1, 1, 6, 1, 1, 1, 6, 1, 6, 1],
+             [1, 6, 6, 1, 6, 1, 1, 1, 6, 1],
+             [1, 1, 6, 1, 6, 6, 6, 6, 6, 1],
+             [1, 1, 6, 6, 1, 6, 1, 1, 6, 1],
+             [1, 1, 1, 1, 1, 5, 1, 1, 1, 1]]
+    a = AStar(init=(0,1), target=(9,5), graph=graph)
+    wall = a.close(x=len(graph), y=len(graph[0]))
+    a.search((0,1), wall, [])
+    print(sorted(a.result)[0][1])
+    
--- ITE4067/MazeRunner.py
+++ ITE4067/MazeRunner.py
@@ -1,47 +1,82 @@
+#!/usr/bin/env python3
 from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import ColorSensor
 import threading, time
 
 class MazeRunner:
-    def __init__(self, sspeed = 5.3, stime = 1, rspeed = 6.8, rtime = 1, mode = 'COL-COLOR'):
+    def __init__(self, sspeed=5.3, stime=1, rspeed=6.8, rtime=1, mode='COL-COLOR'):
         self.sspeed = sspeed
         self.stime = stime
         self.rspeed = rspeed
         self.rtime = rtime
-        self.cmap = ['None', 'Black', 'Blue', 'Green', 'Yellow', 'Red', 'White', 'Brown']
         self.roller = LargeMotor(OUTPUT_A)
         self.scanner = LargeMotor(OUTPUT_B)
         # self.runner = MediumMotor(OUTPUT_C)
         self.cs = ColorSensor(INPUT_1)
         self.cs.mode = mode
         self.maze = []
-        print("{}, {}, {}, {}, {}".format(self.sspeed, self.stime, self.rspeed, self.rtime, self.cs.mode))
+        self.head = 0
+        self.paper = 0
 
-    def navigator(self):
-        for _ in range(5):
-            for _ in range(9):
-                self.scanner.on_for_seconds(self.sspeed, self.stime)
-                self.maze.append(self.cs.value())
-            self.roller.on_for_seconds(self.rspeed, self.rtime)
-            for _ in range(9):
-                self.scanner.on_for_seconds(-self.sspeed, self.stime)
-                self.maze.append(self.cs.value())
-            self.roller.on_for_seconds(self.rspeed, self.rtime)
-        self.roller.on_for_seconds(-self.rspeed, self.rtime * 10)
-            
-    def announcer(self):
+    def navigator(self, line, reset=0):
+        if (reset == 0):
+            if (self.head == 0):
+                for _ in range(10):
+                    self.move(0)
+                    line.append(self.cs.value())
+            elif (self.head == 9):
+                for _ in range(10):
+                    self.move(3)
+                    line.append(self.cs.value())
+        else:
+            for _ in range(self.head):
+                self.move(3)
+            for _ in range(self.paper):
+                self.move(2)
+        return line
+
+    def announcer(self, cmap):
         for l in range(int(len(self.maze)/10) - 1):
             for p in range(10):
                 if (p != 9):
-                    print("{:<6}".format(self.cmap[self.maze[p + l * 10]]), end=" ")
+                    print("{:<6}".format(cmap[self.maze[p + l * 10]]), end=" ")
                 else:
-                    print("{:<6}".format(self.cmap[self.maze[p + l * 10]]))
+                    print("{:<6}".format(cmap[self.maze[p + l * 10]]))
 
-    def 
+    def move(self, direction):
+        if (direction == 0):
+            self.scanner.on_for_seconds(self.sspeed, self.stime)
+            self.head += 1
+        elif (direction == 1):
+            self.roller.on_for_seconds(self.sspeed, self.stime)
+            self.paper += 1
+        elif (direction == 2):
+            self.roller.on_for_seconds(-self.sspeed, self.stime)
+            self.paper -= 1
+        elif (direction == 3):
+            self.scanner.on_for_seconds(self.sspeed, self.stime)
+            self.head -= 1
+    
+    def ending(self, line):
+        for i in line:
+            if i == 5:
+                return True
+        return False
+
+    def running(self, commands):
+        self.navigator([], reset=1)
+        for command in commands:
+            self.move(direction=command)
+    
 
 
-
-mr = MazeRunner(sspeed=5, rspeed=5)
-mr.navigator()
-mr.announcer()
+if __name__ == "__main__":
+    mr = MazeRunner(sspeed=5, rspeed=5)
+    line = mr.navigator(line=[], reset=1)    
+    while(mr.ending(line=line)):
+        line = mr.navigator(line=[])
+        mr.maze.append(line)
+        mr.move(direction=1)
+    mr.announcer(cmap=['None', 'Black', 'Blue', 'Green', 'Yellow', 'Red', 'White', 'Brown'])
+    # mr.running(commands=mr.analyze())
